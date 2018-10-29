7676d3f941211db224612b1cc86de95aa0ab60dd
Ben <bpgerbe@gmail.com>
2018-10-29 15:50:01 -0500

    Having MAJOR troubles

--- hw8/hw8.m
+++ hw8/hw8.m
@@ -2,8 +2,9 @@
 
 G1 = zpk([],[-2 -5 -10 -15],[200]);
 rlocus(G1);
-X1a = evalfr(G1, -1.9 + 3.88*j);
+X1a = evalfr(G1, -1.9100 + 3.7400i)
 k1a = 1/X1a
 
-Gcl1a = minreal(G1*k1a / (1+G1*k1a));
-zpk(Gcl1a)
+Gcl1a = minreal(G1*k1a / (1+G1*k1a))
+
+Gcl1aFinal = zpk([],[-16.9 .5461 (7.826 + 5.207j) (7.826-5.207j)],[-2315.1])

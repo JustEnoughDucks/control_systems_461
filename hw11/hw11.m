a84a406969a42d6b2299b515bfc0f522206dd75c
Ben <bpgerbe@gmail.com>
2018-11-29 10:06:54 -0600

    finished hw11 redo

--- hw11/hw11.m
+++ hw11/hw11.m
@@ -1,53 +1,47 @@
-%% Controls Homework 11 Problem 1
-Gs1 = zpk([],[2 -10 -15 -30],[200]);
+clear
+clc
 
-X1 = evalfr(Gs1, -2)
+%% Controls Homework 11 Problem 1
+Gs1 = zpk([],[0 -10 -15 -50],200);
+
+X1 = evalfr(Gs1, -2.6 + 5.08j)
 K1 = -1/X1
 
 Gcl1 = minreal(Gs1*abs(K1) / (1 + Gs1*abs(K1)))
 
-Gs1_2 = zpk([],[0 -8 -20.24 -28.46],[11648]);
+T = .05;
 
-X1_2 = evalfr(Gs1_2, -2.3302 + 4.5505i)
-K1_2 = -1/X1_2
-
-Gcl1_2 = minreal(Gs1_2*abs(K1_2) / (1 + Gs1_2*abs(K1_2)))
-
-T = .1;
-
-s1 = [roots([1 52.04 697])', roots([1 4.66 26.14])'];
+s1 = [roots([1 4.656 27.64])', roots([1 45.34 536.3])'];
 z1 = exp(s1*T);
 
-Gz1 = zpk([],z1,1);
-Gs1 = zpk([],s1,18216);
-kz1 = evalfr(Gz1, 1);
+Gz1p = zpk([],z1,1,T)
+Gs1 = zpk([],s1,14823)
+kz1 = evalfr(Gz1p, 1);
 ks1 = evalfr(Gs1, 0);
 k1 = ks1/kz1
-Gz1 = zpk([0 0],z1,k1,T)
-Gz1tf = tf(Gz1)
+Gz1 = zpk([0 0],z1,k1,T);
 
-step(Gz1)
-hold on
-step(Gs1)
-hold off
+Kz1 = zpk([0.8187 0.6065],[1 -0.0834],34.7201,.1)
+Kz1 = tf(Kz1)
+
+step(Gcl1)
 
 %% Controls Homework 11 Problem 2
 
 T2 = .250;
 
-s1 = [roots([1 52.04 697])', roots([1 4.66 26.14])'];
-z1 = exp(s1*T2);
+s2 = [roots([1 4.656 27.64])', roots([1 45.34 536.3])'];
+z2 = exp(s2*T2);
 
-Gz1 = zpk([],z1,1);
-Gs1 = zpk([],s1,18216);
-kz1 = evalfr(Gz1, 1);
-ks1 = evalfr(Gs1, 0);
-k1 = ks1/kz1
-Gz1 = zpk([0 0],z1,k1,T2)
-Gz1tf = tf(Gz1)
+Gz2p = zpk([],z2,1,T2);
+Gs2 = zpk([],s2,14823);
+kz2 = evalfr(Gz2p, 1);
+ks2 = evalfr(Gs2, 0);
+k2 = ks2/kz2
+Gz2 = zpk([0 0],z2,k2,T2)
+
+Kz2 = zpk([0.2865 0.6065],[1 -0.7287],31.5021,.25)
+Kz2 = tf(Kz2)
 
 figure;
-step(Gz1)
-hold on
-step(Gs1)
-hold off
+step(Gs2)
